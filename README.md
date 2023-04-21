8f535c3780be24ea71b634f4daf0c18695d223f1
Hallo_Tschuess <hallo.ich.f@gmail.com>
2023-04-21 22:56:02 +0200

    Add new features: digest, temp files and cleaning of empty directories
    Digest supports MD5 and SHA-256
    Update README.md

--- README.md
+++ README.md
@@ -31,19 +31,26 @@ docker run -v my_volume:/uploads -p 80:80 -e URL_BASE_PATH=/my-base-path/ hallot
 | `AUTH_HEADER_PREFIX` | <code>Bearer&nbsp;</code> | Prefix of header (note the space after `Bearer`) |
 | `DEBUG`              | `false`                   | Enable debug log messages                        |
 | `LISTEN`             | `:80`                     | Internal address and port to listen on           |
-| `LOG_FORMAT`         |                           | Logging format: `json` `logfmt` else text mode   |
+| `LOG_FORMAT`         | _text mode_               | Logging format: `json` `logfmt` else _text mode_ |
 | `ROOT_DIR`           | `/uploads`                | Root directory for uploaded files                |
 | `TOKEN_DELETE`       |                           | Token for DELETE method                          |
 | `TOKEN_GET`          |                           | Token for GET method                             |
 | `TOKEN_PUT`          |                           | Token for PUT method                             |
 | `URL_BASE_PATH`      | `/`                       | Base path for URL                                |
+| `FORCE_DIGEST`       | `false`                   | Force the use of a digest for file uploads       |
 
 ### Upload
 
-You can upload a file with `PUT` to the desired location.  
-If a formfile at key `file` is present it gets saved else the request body is used as content.  
-The filename will be ignored. Only the URL-path is important.  
-Any existing file gets overwritten.
+You can upload a file using the `PUT` method to the specified location.  
+The server stores a _formfile_ under the key `file`.  
+If the key is absent, the server uses the _request body_ as the file content.   
+The filename is not considered, only the URL-path matters.  
+If a file already exists at the location, it will be overwritten.
+
+When uploading, a temporary file is created in the same directory as the destination file.  
+If an error occurs, the temporary file is cleaned up. The destination file will not be created/updated.
+
+_Examples:_
 
 Create a file `test.txt` with content `Hello world`:
 
@@ -57,10 +64,19 @@ Upload `./my/file.jpg` as `/some/dir/my-picture.jpg`:
 curl -X PUT -F "file=@my/file.jpg" example.com/some/dir/my-picture.jpg
 ```
 
+You can use the [Digest header](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Digest) to verify file
+integrity.  
+Supported hashing functions are `MD5` and `SHA-256`.   
+All provided digest values are evaluated.
+
+```shell
+curl -X PUT -H "Content-Type: text/plain" -H "Digest: md5=PiWWCnnbxptnTNTsZ6csYg==,sha-256=ZOyIygCyaOW6GjVnihtTFtIS9PNmskdyMlNKiuyjfzw=" -d "Hello world" example.com/test.txt
+```
+
 ### Delete
 
 You can delete a file or directory (recursively) with `DELETE`.  
-Empty folders won't be deleted automatically.
+Empty folders are automatically removed.
 
 ```shell
 curl -X DELETE example.com/test.txt
@@ -68,15 +84,16 @@ curl -X DELETE example.com/test.txt
 
 ## Security
 
-CORS is always enabled: `Access-Control-Allow-Origin: *`.
+> Notice: CORS is always enabled: `Access-Control-Allow-Origin: *`.
 
 The server implements a very simple token auth per method.  
-A token can be passed by the `token` query parameter or a header set with `AUTH_HEADER` and `AUTH_HEADER_PREFIX`.  
-If token is provided both ways the query parameter has precedence.  
-By default, the header is `Authorization` the token is `Bearer <token>`.
+You can pass the token either as a `token` query parameter or via a header defined by `AUTH_HEADER`
+and `AUTH_HEADER_PREFIX`.  
+If a token is provided both ways, the query parameter takes precedence.  
+By default, the header is `Authorization` the token is format `Bearer <token>`.
 
 _Example:_  
-To restrict access to PUT requests set the environment variable TOKEN_PUT to your desired token.  
+To restrict access to PUT requests, set the environment variable `TOKEN_PUT` to your desired token.  
 Now you have to specify this token with your request parameters.
 
 ```shell
--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"hash"
 	"io"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -18,6 +26,7 @@ var (
 	logFormat        = os.Getenv("LOG_FORMAT")
 	authHeader       = os.Getenv("AUTH_HEADER")
 	authHeaderPrefix = os.Getenv("AUTH_HEADER_PREFIX")
+	forceDigest, _   = strconv.ParseBool(os.Getenv("FORCE_DIGEST"))
 )
 
 var tokens = map[string]string{
@@ -25,6 +34,8 @@ var tokens = map[string]string{
 	http.MethodPut:    os.Getenv("TOKEN_PUT"),
 	http.MethodDelete: os.Getenv("TOKEN_DELETE"),
 }
+
+var allowedMethods = []string{http.MethodOptions, http.MethodGet, http.MethodPut, http.MethodDelete}
 
 func init() {
 	if rootDir == "" {
@@ -61,7 +72,6 @@ func init() {
 	if authHeader == "" {
 		authHeader = "Authorization"
 	}
-
 	if authHeaderPrefix == "" {
 		authHeaderPrefix = "Bearer "
 	}
@@ -79,7 +89,6 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 		log.Debugf("Unsupported method %s for /", r.Method)
 		w.Header().Set("Allow", http.MethodGet)
 		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
-
 		return
 	}
 
@@ -90,24 +99,20 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 			if !strings.HasPrefix(r.Header.Get(authHeader), authHeaderPrefix) {
 				log.Debugf("Wrong header prefix for %s: %s", r.Method, r.URL.Path)
 				http.Error(w, "Wrong header prefix", http.StatusUnauthorized)
-
 				return
 			}
-
 			token = strings.TrimPrefix(r.Header.Get(authHeader), authHeaderPrefix)
 		}
 
 		if token == "" {
 			log.Debugf("Missing token for %s: %s", r.Method, r.URL.Path)
 			http.Error(w, "Missing token", http.StatusUnauthorized)
-
 			return
 		}
 
 		if tokens[r.Method] != token {
 			log.Debugf("Wrong token for %s: %s", r.Method, r.URL.Path)
 			http.Error(w, "Wrong token", http.StatusUnauthorized)
-
 			return
 		}
 	}
@@ -115,6 +120,8 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	log.Debugf("%s: %s", r.Method, r.URL.Path)
 
 	switch r.Method {
+	case http.MethodOptions:
+		w.Header().Set("Allow", strings.Join(allowedMethods, ", "))
 	case http.MethodGet:
 		http.StripPrefix(urlBasePath, http.FileServer(http.Dir(rootDir))).ServeHTTP(w, r)
 	case http.MethodPut:
@@ -122,33 +129,27 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	case http.MethodDelete:
 		handleDelete(w, r)
 	default:
-		log.Debug("Unsupported method %s for %s", r.Method, r.URL.Path)
-		w.Header().Set("Allow", strings.Join([]string{http.MethodGet, http.MethodPut, http.MethodDelete}, ", "))
+		log.Debugf("Unsupported method %s for %s", r.Method, r.URL.Path)
+		w.Header().Set("Allow", strings.Join(allowedMethods, ", "))
 		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
 	}
 }
 
 func handlePut(w http.ResponseWriter, r *http.Request) {
-	filePath := urlToPath(r.URL.Path)
+	w = &ignoreMultiWriteHeaderWriter{ResponseWriter: w}
+	defer w.WriteHeader(http.StatusNoContent)
 
-	err := os.MkdirAll(path.Dir(filePath), 0700)
+	digest, ok, err := parseDigest(r.Header.Get("Digest"))
 	if err != nil {
-		log.Warnf("Error while creating directory for file '%s': %s'", filePath, err)
-		http.Error(w,
-			"Path could not be created. Make sure the path is correct",
-			http.StatusInternalServerError)
-
+		log.Errorf("Error while parsing digest: %s", err)
+		http.Error(w, "Digest could not be parsed \n\n"+err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Errorf("Error while opening file '%s': %s", filePath, err)
-		http.Error(w, "File could not be created/opened", http.StatusInternalServerError)
-
+	if forceDigest && !ok {
+		log.Errorf("Missing digest")
+		http.Error(w, "Missing digest. Supported algorithms: sha-256, md5", http.StatusBadRequest)
 		return
 	}
-	defer file.Close()
 
 	var fileReader io.Reader
 
@@ -160,17 +161,57 @@ func handlePut(w http.ResponseWriter, r *http.Request) {
 		fileReader = formFile
 	}
 
-	_, err = io.Copy(file, fileReader)
-	if err != nil {
-		log.Errorf("Error while writing file '%s': %s", filePath, err)
-		http.Error(w, "File could not be saved", http.StatusInternalServerError)
+	subjectPath := urlToPath(r.URL.Path)
 
+	err = os.MkdirAll(path.Dir(subjectPath), 0700)
+	if err != nil {
+		log.Warnf("Error while creating directory for file '%s': %s'", subjectPath, err)
+		http.Error(w, "Path could not be created. Make sure the path is correct", http.StatusInternalServerError)
+		return
+	}
+	var rollback multiFunc
+	rollback.add(func() {
+		if err := cleanEmptyDir(rootDir, filepath.Dir(subjectPath)); err != nil {
+			log.Errorf("Error cleaning empty directories: %s", err)
+		}
+	})
+
+	file, err := newTransactionFile(subjectPath)
+	if err != nil {
+		log.Errorf("Error while opening file '%s': %s", subjectPath, err)
+		http.Error(w, "File could not be created/opened", http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Errorf("Error while closing file '%s': %s", subjectPath, err)
+			http.Error(w, "File could not be closed", http.StatusInternalServerError)
+		}
+	}()
+	rollback.add(func() {
+		if err := file.rollback(); err != nil {
+			log.Errorf("Error while rolling back file '%s': %s", subjectPath, err)
+		}
+	})
+
+	out := io.MultiWriter(file, digest)
+
+	_, err = io.Copy(out, fileReader)
+	if err != nil {
+		log.Errorf("Error while writing file '%s': %s", subjectPath, err)
+		http.Error(w, "File could not be saved", http.StatusInternalServerError)
+		rollback.run()
 		return
 	}
 
-	log.Debugf("File '%s' written", filePath)
+	if !digest.isValid() {
+		log.Warnf("Invalid digest for file '%s'", subjectPath)
+		http.Error(w, "Invalid digest. Supported algorithms: sha-256, md5", http.StatusBadRequest)
+		rollback.run()
+		return
+	}
 
-	w.WriteHeader(http.StatusNoContent)
+	log.Debugf("File '%s' written", subjectPath)
 }
 
 func handleDelete(w http.ResponseWriter, r *http.Request) {
@@ -180,13 +221,11 @@ func handleDelete(w http.ResponseWriter, r *http.Request) {
 	if os.IsNotExist(err) {
 		log.Debugf("File '%s' not found", subjectPath)
 		http.NotFound(w, r)
-
 		return
 	}
 	if err != nil {
 		log.Errorf("Error with file stats '%s': %s", subjectPath, err)
 		http.Error(w, "File could not be deleted", http.StatusInternalServerError)
-
 		return
 	}
 
@@ -194,7 +233,12 @@ func handleDelete(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Errorf("Error while removing file '%s': %s", subjectPath, err)
 		http.Error(w, "File could not be deleted", http.StatusInternalServerError)
+		return
+	}
 
+	if err := cleanEmptyDir(rootDir, filepath.Dir(subjectPath)); err != nil {
+		log.Errorf("Error cleaning empty directories: %s", err)
+		http.Error(w, "Could clean empty directories", http.StatusInternalServerError)
 		return
 	}
 
@@ -204,5 +248,230 @@ func handleDelete(w http.ResponseWriter, r *http.Request) {
 }
 
 func urlToPath(urlPath string) string {
-	return path.Join(rootDir, strings.TrimPrefix(urlPath, urlBasePath))
+	return path.Join(rootDir, strings.TrimPrefix(path.Clean(urlPath), urlBasePath))
+}
+
+func cleanEmptyDir(root, path string) error {
+	for !isPathEqual(root, path) {
+		empty, err := isDirEmpty(path)
+		if err != nil {
+			return fmt.Errorf("empty directory check '%s': %w", path, err)
+		}
+
+		if !empty {
+			return nil
+		}
+
+		log.Debug("Delete parent dir ")
+		if err := os.Remove(path); err != nil {
+			return fmt.Errorf("delete directory '%s': %s", path, err)
+		}
+		path = filepath.Dir(path)
+	}
+	return nil
+}
+
+func isDirEmpty(path string) (bool, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return false, fmt.Errorf("directory open: %w", err)
+	}
+	defer f.Close()
+	_, err = f.ReadDir(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err
+}
+
+func isPathEqual(path1, path2 string) bool {
+	p1, err := filepath.Abs(path1)
+	if err != nil {
+		return false
+	}
+	p2, err := filepath.Abs(path2)
+	if err != nil {
+		return false
+	}
+	return p1 == p2
+}
+
+type transactionFile struct {
+	*os.File
+	target string
+	done   bool
+}
+
+func newTransactionFile(p string) (*transactionFile, error) {
+	f, err := os.CreateTemp(filepath.Dir(p), "")
+	if err != nil {
+		return nil, err
+	}
+	return &transactionFile{
+		File:   f,
+		target: p,
+	}, nil
+}
+
+func (f *transactionFile) Close() error {
+	return f.commit()
+}
+
+func (f *transactionFile) commit() error {
+	if f.done {
+		return nil
+	}
+	f.done = true
+	if err := f.File.Close(); err != nil {
+		return fmt.Errorf("close temp file: %w", err)
+	}
+
+	err := os.Rename(f.File.Name(), f.target)
+	if err == nil {
+		return nil
+	}
+	if !os.IsExist(err) {
+		return fmt.Errorf("rename temp file: %w", err)
+	}
+
+	if err := os.Remove(f.target); err != nil {
+		return fmt.Errorf("delete target file: %w", err)
+	}
+	if err := os.Rename(f.File.Name(), f.target); err != nil {
+		return fmt.Errorf("rename temp file: %w", err)
+	}
+
+	return nil
+}
+
+func (f *transactionFile) rollback() error {
+	if f.done {
+		return nil
+	}
+	f.done = true
+	if err := f.File.Close(); err != nil {
+		return fmt.Errorf("close temp file: %w", err)
+	}
+	if err := os.Remove(f.File.Name()); err != nil {
+		return fmt.Errorf("remove temp file: %w", err)
+	}
+	return nil
+}
+
+type ignoreMultiWriteHeaderWriter struct {
+	http.ResponseWriter
+	headerWritten bool
+}
+
+func (w *ignoreMultiWriteHeaderWriter) WriteHeader(code int) {
+	if w.headerWritten {
+		return
+	}
+	w.ResponseWriter.WriteHeader(code)
+	w.headerWritten = true
+}
+
+type multiFunc struct {
+	fns []func()
+}
+
+func (r *multiFunc) add(fn func()) {
+	r.fns = append(r.fns, fn)
+}
+
+func (r *multiFunc) run() {
+	for _, fn := range r.fns {
+		fn()
+	}
+}
+
+type digestValidator interface {
+	io.Writer
+	isValid() bool
+}
+
+var digestValidators = map[string]func(string) (digestValidator, error){
+	"sha-256": newSHA256Validator,
+	"md5":     newMD5Validator,
+}
+
+func parseDigest(s string) (digestValidator, bool, error) {
+	parts := strings.Split(s, ",")
+	validator := &multiDigestValidator{
+		Writer:     io.MultiWriter(),
+		validators: make([]digestValidator, 0, len(parts)),
+	}
+
+	var errs []error
+	for _, part := range parts {
+		algo, val, ok := strings.Cut(part, "=")
+		if !ok {
+			continue
+		}
+
+		if fn, ok := digestValidators[algo]; ok {
+			v, err := fn(val)
+			if err != nil {
+				errs = append(errs, err)
+				continue
+			}
+			validator.Writer = io.MultiWriter(validator.Writer, v)
+			validator.validators = append(validator.validators, v)
+		}
+	}
+
+	return validator, len(validator.validators) > 0, errors.Join(errs...)
+}
+
+type multiDigestValidator struct {
+	io.Writer
+	validators []digestValidator
+}
+
+func (v *multiDigestValidator) isValid() bool {
+	for _, validator := range v.validators {
+		if !validator.isValid() {
+			return false
+		}
+	}
+	return true
+}
+
+type hashValidator struct {
+	digest []byte
+	hasher hash.Hash
+}
+
+func (v *hashValidator) Write(p []byte) (int, error) {
+	return v.hasher.Write(p)
+}
+
+func (v *hashValidator) isValid() bool {
+	sum := v.hasher.Sum(nil)
+	if bytes.Equal(sum, v.digest) {
+		return true
+	}
+	return false
+}
+
+func newSHA256Validator(digest string) (digestValidator, error) {
+	d, err := base64.StdEncoding.DecodeString(digest)
+	if err != nil {
+		return nil, err
+	}
+	return &hashValidator{
+		digest: d,
+		hasher: sha256.New(),
+	}, nil
+}
+
+func newMD5Validator(digest string) (digestValidator, error) {
+	d, err := base64.StdEncoding.DecodeString(digest)
+	if err != nil {
+		return nil, err
+	}
+	return &hashValidator{
+		digest: d,
+		hasher: md5.New(),
+	}, nil
 }
